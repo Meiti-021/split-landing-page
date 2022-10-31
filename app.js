cb1527023bdfc63c43d69adaaec5cce27cbca2d6
Mahdi <meiti.edu@gmail.com>
2022-11-01 01:24:44 +0330

    refactor js and make page responsive

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ let $ = document;
 
 const playStation = $.querySelector('.play-station');
 const xBox = $.querySelector('.x-box');
+const sides = $.querySelectorAll('.sides')
 
 playStation.addEventListener('mouseenter', ()=>{
-    playStation.style.width = '80%';
-    xBox.style.width = '20%';
+    sides.forEach((elem)=>{
+        elem.classList.remove('active');
+        elem.classList.remove('de-active')
+    })
+    playStation.classList.add('active');
+    xBox.classList.add('de-active');
 })
 xBox.addEventListener('mouseenter', ()=>{
-    xBox.style.width = '80%';
-    playStation.style.width = '20%';
+    sides.forEach((elem)=>{
+        elem.classList.remove('active');
+        elem.classList.remove('de-active')
+    })
+    xBox.classList.add('active');
+    playStation.classList.add('de-active');
 })
